70fb73775eb5a8aec8620928bf7deb1b6870cd17
roxuz <roxuzopi@gmail.com>
2022-06-27 10:50:54 +0200

    start_ords.sh

--- start_ords.sh
+++ start_ords.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 export PATH=/usr/sbin:/usr/local/bin:/usr/bin:/usr/local/sbin:$PATH
-#export JAVA_HOME=/usr
 export JAVA_HOME=/u03/java/latest
-#LOGFILE=/home/oracle/scripts/logs/ords-`date +"%Y""%m""%d"`.log
-LOGFILE=/u03/config/instance1/logs/ords-`date +"%Y""%m""%d"`.log
-cd /u03/ords
 export JAVA_OPTIONS="-Dorg.eclipse.jetty.server.Request.maxFormContentSize=3000000 -Duser.timezone=UTC"
+
+LOGFILE=/u03/config/instance1/logs/ords-`date +"%Y""%m""%d"`.log
+
+cd /u03/ords
 nohup $JAVA_HOME/bin/java ${JAVA_OPTIONS} -jar ords.war standalone >> $LOGFILE 2>&1 &
 echo "View log file with : tail -f $LOGFILE"
